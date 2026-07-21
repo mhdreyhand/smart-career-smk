786267c4d2454ba3f170d809957f1664acbba31c
Muhammad Reyhan <150309720+mhdreyhand@users.noreply.github.com>
2026-07-21 11:38:49 +0700

    Add KOMPETENSI_KESTA dictionary to kompetensi.py

---
+++ kompetensi.py
@@ -0,0 +1,21 @@
+# kompetensi.py
+
+KOMPETENSI_KESTA = {
+    "Dasar-Dasar Program Keahlian (Kompetensi Umum & Inti)": [
+        "Mengumpulkan kebutuhan teknis pengguna jaringan",
+        "Mengumpulkan data peralatan jaringan dengan teknologi yang sesuai",
+        "Menyiapkan kabel jaringan",
+        "Memasang kabel jaringan",
+        "Menentukan spesifikasi perangkat jaringan",
+        "Memasang jaringan nirkabel"
+    ],
+    "Konsentrasi Keahlian (Kompetensi Fungsional)": [
+        "Merancang topologi jaringan",
+        "Merancang pengalamatan jaringan",
+        "Mengkonfigurasi switch pada jaringan",
+        "Memasang perangkat jaringan ke dalam sistem jaringan",
+        "Mengkonfigurasi routing pada perangkat jaringan dalam satu Autonomous System",
+        "Memonitor keamanan dan pengaturan akun pengguna dalam jaringan komputer",
+        "Mengganti perangkat jaringan sesuai dengan kebutuhan baru"
+    ]
+}
