921c7d5007d3530af586774027f990e28ff0a13b
Muhammad Reyhan <150309720+mhdreyhand@users.noreply.github.com>
2026-07-21 11:51:10 +0700

    Add Praktik Kerja Lapangan (PKL) tasks to kompetensi.py

--- kompetensi.py
+++ kompetensi.py
@@ -17,5 +17,20 @@ KOMPETENSI_KESTA = {
         "Mengkonfigurasi routing pada perangkat jaringan dalam satu Autonomous System",
         "Memonitor keamanan dan pengaturan akun pengguna dalam jaringan komputer",
         "Mengganti perangkat jaringan sesuai dengan kebutuhan baru"
+    ],
+    "Praktik Kerja Lapangan (PKL)": [
+        "Mengumpulkan kebutuhan teknis pengguna yang menggunakan jaringan",
+        "Mengumpulkan data peralatan jaringan dengan teknologi yang sesuai",
+        "Menyiapkan kabel jaringan",
+        "Memasang kabel jaringan",
+        "Menentukan spesifikasi perangkat jaringan",
+        "Memasang jaringan nirkabel",
+        "Merancang topologi jaringan",
+        "Merancang pengalamatan jaringan",
+        "Mengkonfigurasi switch pada jaringan",
+        "Memasang perangkat jaringan ke dalam sistem jaringan",
+        "Mengkonfigurasi routing pada perangkat jaringan dalam satu Autonomous System",
+        "Memonitor keamanan dan pengaturan akun pengguna dalam jaringan komputer",
+        "Mengganti perangkat jaringan sesuai dengan kebutuhan baru"
     ]
 }
